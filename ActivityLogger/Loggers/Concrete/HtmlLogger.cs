6261cbc38fb3863c052dec98c9249ab40a33938b
wkallhof <shagler@gmail.com>
2013-12-10 21:06:09 -0600

    Adding HTML Templating

--- ActivityLogger/Loggers/Concrete/HtmlLogger.cs
+++ ActivityLogger/Loggers/Concrete/HtmlLogger.cs
@@ -32,14 +32,18 @@ namespace ActivityLogger.Loggers.Concrete
         {
             try
             {
-                var writer = new StreamWriter(filePath, false);
-                writer.WriteLine("<html><head></head><body>");
-                writer.WriteLine("<table><th>Application Name</th><th>Keys Pressed</th>");
-                var content = String.Join("", logData.Select(x => WriteLogLine(x.Key, x.Value)));
-                writer.WriteLine(content);
-                writer.WriteLine("</table>");
-                writer.WriteLine("</body></html>");
+                var template = FetchTemplateString();
+                var markupBuilder = new StringBuilder();
 
+                markupBuilder.AppendLine("<table><th>Application Name</th><th>Keys Pressed</th>");
+                var content = String.Join("", logData.Select(x => WriteLogLine(x.Key, x.Value)));
+                markupBuilder.AppendLine(content);
+                markupBuilder.AppendLine("</table>");
+
+                var outputHtml = template.Replace("{content}", markupBuilder.ToString());
+
+                var writer = new StreamWriter(filePath, false);
+                writer.WriteLine(outputHtml);
                 writer.Flush();
                 writer.Close();
             }
@@ -47,6 +51,16 @@ namespace ActivityLogger.Loggers.Concrete
             {
                 Debug.WriteLine(ex.Message, ex.StackTrace);
             }
+        }
+
+        private string FetchTemplateString()
+        {
+            var templatePath = Path.Combine(Application.StartupPath, "Loggers","Concrete","HtmlLogTemplate.html");
+            StreamReader sr = new StreamReader(templatePath);
+            string template = sr.ReadLine();
+            sr.Close();
+
+            return template;
         }
 
         private string WriteLogLine(string application, string appLog)
